9ff29c58265ebbaaa9083452bcd609bd5aa273e2
ERIC KELLY <eakelly220@gmail.com>
2018-06-25 05:49:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,18 @@ class Student
   attr_accessor :name, :grade
   
   attr_reader :id
+
+  namespace :greeting do 
+desc 'outputs hello to the terminal'
+  task :hello do 
+    puts "hello from Rake!"
+  end
+ 
+  desc 'outputs hola to the terminal'
+  task :hola do 
+    puts "hola de Rake!"
+  end
+end
   
   def initialize(name, grade, id=nil)
     @id = id
